4335cefa541c96381f612c9740cab600bad80e6f
bishtyyy8 <87144590+bishtyyy8@users.noreply.github.com>
2022-01-13 23:57:52 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,18 +1,20 @@
+// Write a Program to print mirrored right triangle star pattern.
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	int i, j, k, rows;
+	int i, j, k, n;
      
-    cout << "Enter Mirrored Right Triangle Star Pattern Row = ";
-    cin >> rows;
+    cout << "Enter Mirrored Right Triangle Star Pattern rows= ";
+    cin >> n;
 
-    cout << "Mirrored Right Angled Triangle Star Pattern\n"; 
+    cout << "Mirrored Right Angled Triangle Pattern\n"; 
 
-    for(i = 1; i <= rows; i++)
+    for(i = 1; i <= n; i++)
     {
-    	for(j = 0; j <= rows - i; j++)
+    	for(j = 0; j <=n - i; j++)
 		{
             cout << " ";
         }
