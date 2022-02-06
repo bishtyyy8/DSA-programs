e164349af5c31b5af65080f9708d74501ea9c1ac
bishtyyy8 <87144590+bishtyyy8@users.noreply.github.com>
2022-02-06 19:08:02 +0530

    Add files via upload

---
+++ pattern4.cpp
@@ -0,0 +1,24 @@
+// Write a Program to print inverted half pyramid star pattern.//
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cout<<"enter the number of lines";
+cin>>n;
+
+cout<<" mirrored and invertedright angled triangle pattern\n";
+
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-1;j++){
+        cout<<"*";
+    }
+    for(int k=0;k=n-1;k++){
+    
+    cout<<"\n";
+    }
+}
+    return 0;
+}
